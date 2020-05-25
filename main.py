b0d9d4828240e9690206f51733a77df437b564e7
Ido Calman <calman.ido@gmail.com>
2020-05-25 12:05:20 +0300

    CLI support

--- main.py
+++ main.py
@@ -3,23 +3,16 @@ import string
 import torch
 from transformers import BartForConditionalGeneration, BartTokenizer
 from dataset import QADataset
+import argparse
 
 
-def load_model(state_dict):
+def load_model(model, state_dict):
     def _convert(key):
         if key.startswith('module.'):
             return key[len('module.'):]
         return key
     single_gpu_state_dict = {_convert(key): value for key, value in state_dict.items()}
-    return BartForConditionalGeneration.from_pretrained('bart-large', state_dict=single_gpu_state_dict)
-
-
-TRAINED_BART_PATH = "out/best-model.pt"
-TEST_DATASET_PATH = "data/nqopen-dev.json"
-PREDICT_BATCH_SIZE = 2
-bart_state_dict = torch.load(TRAINED_BART_PATH)
-bart = load_model(bart_state_dict)
-tokenizer = BartTokenizer.from_pretrained('bart-large')
+    return BartForConditionalGeneration.from_pretrained(model, state_dict=single_gpu_state_dict)
 
 
 def normalize_answer(s):
@@ -38,28 +31,52 @@ def normalize_answer(s):
     return white_space_fix(remove_articles(remove_punc(lower(s))))
 
 
-def predict(question):
-    questions_encoded = tokenizer.batch_encode_plus(question, pad_to_max_length=True, max_length=32, return_tensors='pt')
+def predict(args, p_model, p_tokenizer, question):
+    questions_encoded = p_tokenizer.batch_encode_plus(question,
+                                                      pad_to_max_length=True,
+                                                      max_length=args.max_length,
+                                                      return_tensors='pt')
     input_ids, attention_mask = questions_encoded["input_ids"], questions_encoded["attention_mask"]
-    outputs = bart.generate(input_ids=input_ids,
-                            attention_mask=attention_mask,
-                            num_beams=4,
-                            max_length=20,
-                            early_stopping=True)
-    predictions = [tokenizer.decode(o, skip_special_tokens=True)[1:] for o in outputs]
+    outputs = p_model.generate(input_ids=input_ids,
+                               attention_mask=attention_mask,
+                               num_beams=4,
+                               max_length=20,
+                               early_stopping=True)
+    predictions = [p_tokenizer.decode(o, skip_special_tokens=True)[1:] for o in outputs]
     return predictions
 
 
+def run(args):
+    # initialize pretrained model and tokenizer from files
+    model_state_dict = torch.load(args.model_file)
+    model = load_model(args.model, model_state_dict)
+    tokenizer = BartTokenizer.from_pretrained(args.model)
+
+    # load predict file data
+    dataset = QADataset(args.predict_file, tokenizer)
+    loader = dataset.loader(batch_size=args.predict_batch_size)
+
+    # run and print model predictions
+    for questions, answers in loader:
+        model_predictions = [normalize_answer(p) for p in predict(args, model, tokenizer, questions)]
+        norm_answers = [normalize_answer(a) for a in answers]
+        for i in range(args.predict_batch_size):
+            print(f"Q: {questions[i]}\nA: {norm_answers[i]}\nP: {model_predictions[i]}\n{'-' * 30}")
+
+
+def main():
+    # parse args from CLI
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default='bart-large')
+    parser.add_argument("--model_file", default="out/best-model.pt")
+    parser.add_argument("--predict_file", default="data/nqopen-test.json")
+    parser.add_argument("--predict_batch_size", default=1, type=int)
+    parser.add_argument("--max_length", default=32, type=int)
+    args = parser.parse_args()
+
+    # run predictions with args
+    run(args)
+
+
 if __name__ == '__main__':
-    dataset = QADataset(TEST_DATASET_PATH, tokenizer)
-    loader = dataset.loader(batch_size=PREDICT_BATCH_SIZE)
-    questions = ["where are the pyramids?",
-                 "who was the president of the united states in 1997?"]
-    model_predictions = predict(questions)
-    print(f"Q: {questions[0]}\n\nP: {model_predictions[0]}\n{'-' * 30}")
-    print(f"Q: {questions[1]}\n\nP: {model_predictions[1]}\n{'-' * 30}")
-    # for questions, answers in loader:
-    #     model_predictions = [normalize_answer(p) for p in predict(questions)]
-    #     norm_answers = [normalize_answer(a) for a in answers]
-    #     for i in range(PREDICT_BATCH_SIZE):
-    #         print(f"Q: {questions[i]}\nA: {norm_answers[i]}\nP: {model_predictions[i]}\n{'-'*30}")
+    main()
